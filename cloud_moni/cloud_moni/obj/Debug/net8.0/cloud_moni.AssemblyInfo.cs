ffc14e77603a9f2578f08a056564c2cc96911145
K3r1n24 <kerindepaz11@gmail.com>
2024-12-01 10:34:04 -0600

    proyecto de prograI my cloud moni

--- cloud_moni/cloud_moni/obj/Debug/net8.0/cloud_moni.AssemblyInfo.cs
+++ cloud_moni/cloud_moni/obj/Debug/net8.0/cloud_moni.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cloud_moni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de9e8412026b37dadff970d8fc17ae5fbd13b01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff523e79ba28ebdea350e0ce79348b2aab8f4ab9")]
 [assembly: System.Reflection.AssemblyProductAttribute("cloud_moni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cloud_moni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
